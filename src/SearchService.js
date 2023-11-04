149e6d07c05cff3eac78e4a30d1ea610665aeac9
maxDes23 <133263504+maxDes23@users.noreply.github.com>
2023-11-04 16:45:29 +0200

    dz11

---
+++ src/SearchService.js
@@ -0,0 +1,21 @@
+
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '40461807-ebea3baaa711ea52e01789531';
+
+async function SearchService(currentPage, searchQuery) {
+  const parameters = new URLSearchParams({
+    key: API_KEY,
+    image_type: `photo`,
+    orientation: `horizontal`,
+    safesearch: `true`,
+    per_page: '40',
+    q: searchQuery,
+    page: currentPage,
+  });
+
+  return axios.get(`${BASE_URL}?${parameters}`);
+}
+
+export { SearchService };
---
+++ src/index.js
@@ -0,0 +1,145 @@
+// index.js
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { SearchService } from './SearchService';
+
+const elements = {
+  form: document.querySelector('.search-form'),
+  cardList: document.querySelector('.gallery'),
+  message: document.querySelector('.message'),
+};
+
+const gallery = new SimpleLightbox('.gallery a');
+let quantityImg = 0;
+let currentPage = 1;
+let loading = false; // Додаємо змінну для відстеження завантаження
+
+elements.form.addEventListener('submit', handleSubmit);
+window.addEventListener('scroll', handleScroll);
+
+async function handleSubmit(evt) {
+  evt.preventDefault();
+  elements.cardList.innerHTML = '';
+  currentPage = 1;
+  quantityImg = 0;
+  loading = false;
+
+  const searchQuery = evt.target.elements.searchQuery.value.trim();
+
+  if (!searchQuery) {
+    return Notify.failure('Enter your search details.');
+  }
+
+  localStorage.setItem('input-value', searchQuery);
+
+  loadImages(searchQuery);
+}
+
+async function loadImages(searchQuery) {
+  try {
+    const data = await SearchService(currentPage, searchQuery);
+
+    if (data.data.totalHits === 0) {
+      elements.message.style.display = 'block';
+      return;
+    }
+
+    quantityImg += data.data.hits.length;
+
+    elements.cardList.insertAdjacentHTML(
+      'beforeend',
+      cardListMarkup(data.data.hits)
+    );
+
+    if (data.data.totalHits !== 0) {
+      Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
+    }
+
+    if (data.data.totalHits > quantityImg) {
+      loading = false; // Позначте, що завантаження завершилося
+    } else {
+      Notify.info("Sorry, but you've reached the end of search results.");
+    }
+
+    if (elements.cardList.firstElementChild) {
+      const { height: cardHeight } =
+        elements.cardList.firstElementChild.getBoundingClientRect();
+      window.scrollBy({
+        top: cardHeight * 2,
+        behavior: 'smooth',
+      });
+    }
+  } catch (error) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } finally {
+    gallery.refresh();
+  }
+}
+
+function cardListMarkup(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class ="photo-card"> 
+        <a class="gallery-link" href="${largeImageURL}"> 
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-likes">
+              <b>Likes: <span class= "item-text">${likes}</span></b>
+            </p>
+            <p class="info-views">
+              <b>Views: <span class= "item-text">${views}</span></b>
+            </p>
+            <p class="info-comments">
+              <b>Comments: <span class= "item-text">${comments}</span></b>
+            </p>
+            <p class="info-downloads">
+              <b>Downloads: <span class= "item-text">${downloads}</span></b>
+            </p>
+          </div>
+        </a>
+      </div>`;
+      }
+    )
+    .join('');
+}
+
+function handleScroll() {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+    !loading
+  ) {
+    loading = true;
+    currentPage++;
+    const inputValue = localStorage.getItem('input-value');
+    loadImages(inputValue);
+  }
+}
+
+const backToTopButton = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
